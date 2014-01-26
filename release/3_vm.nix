d41d12d6ee83cd66f2413a6cfc55548f63a9590f
Matej Cotman <cotman.matej@gmail.com>
2014-01-26 08:41:35 +0100

    Update 3_vm.nix

--- release/3_vm.nix
+++ release/3_vm.nix
@@ -48,7 +48,7 @@ let
     
     export NIX_OTHER_STORES=/nix/store
 
-    nix-build ${<hydra_scripts/release/vm_build.nix>} -A vmEnvironment --argstr nixpkgs ${vmNixpkgs.outPath} --argstr prefix ${prefixDir} --argstr attrs_str "${attrs_str}" --argstr system ${system} --show-trace
+    echo nix-build ${<hydra_scripts/release/vm_build.nix>} -A vmEnvironment --argstr nixpkgs ${vmNixpkgs.outPath} --argstr prefix ${prefixDir} --argstr attrs_str "${attrs_str}" --argstr system ${system} --show-trace
 
     test -L ./result && cp -Pv ./result ${prefixDir}
 
