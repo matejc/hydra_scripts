7e3a9d188aecad227fa48c2fb2375155f24857e1
Matej Cotman <cotman.matej@gmail.com>
2014-03-06 22:40:40 +0100

    Update 5_vm_repo.nix

--- release/5_vm_repo.nix
+++ release/5_vm_repo.nix
@@ -55,7 +55,7 @@ let
     mkdir ${prefixDir}/var/nix/db
     export NIX_DB_DIR=${prefixDir}/var/nix/db
 
-    nix-build ${<hydra_scripts/release/vm_build_repo.nix>} -A build --argstr nixpkgs ${<nixpkgs>} --argstr hydra_scripts ${hydra_scripts.outPath} --argstr prefix ${prefixDir} --argstr build_inputs_str "${build_inputs_str}" --argstr system ${system} --argstr package_name "${esc package_name}" --argstr package_repo "${esc package_repo}" --argstr build_command "${esc build_command}" --argstr dist_command "${esc dist_command}" --argstr check_command "${esc check_command}" --argstr dist_path "${esc dist_path}" --argstr docs_path "${esc docs_path}" --argstr cov_command "${esc cov_command}" --argstr source_files "${esc source_files}" --argstr binary_files "${esc binary_files}" --argstr install_command "${esc install_command}" --argstr with_vnc_command "${esc with_vnc_command}" -vvv --show-trace
+    nix-build ${<hydra_scripts/release/vm_build_repo.nix>} -A build --argstr nixpkgs ${<nixpkgs>} --argstr hydra_scripts ${<hydra_scripts>} --argstr prefix ${prefixDir} --argstr build_inputs_str "${build_inputs_str}" --argstr system ${system} --argstr package_name "${esc package_name}" --argstr package_repo "${esc package_repo}" --argstr build_command "${esc build_command}" --argstr dist_command "${esc dist_command}" --argstr check_command "${esc check_command}" --argstr dist_path "${esc dist_path}" --argstr docs_path "${esc docs_path}" --argstr cov_command "${esc cov_command}" --argstr source_files "${esc source_files}" --argstr binary_files "${esc binary_files}" --argstr install_command "${esc install_command}" --argstr with_vnc_command "${esc with_vnc_command}" -vvv --show-trace
 
     echo $? > /tmp/xchg/exitstatuscode
 
