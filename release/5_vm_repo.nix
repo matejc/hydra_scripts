d618485afe1b44691b1605935106ee912c0ed417
Matej Cotman <cotman.matej@gmail.com>
2014-03-06 21:52:31 +0100

    Update 5_vm_repo.nix
    add group write permission

--- release/5_vm_repo.nix
+++ release/5_vm_repo.nix
@@ -104,6 +104,7 @@ let
     while `test -f /var/images/$HASH.lock`; do sleep 1; done
     touch /var/images/$HASH.lock
     export NIX_DISK_IMAGE=/var/images/$HASH.img
+    chmod g+w $NIX_DISK_IMAGE
     timeout ${vm_timeout} ${vm.config.system.build.vm}/bin/run-*-vm
     rm /var/images/$HASH.lock
 
