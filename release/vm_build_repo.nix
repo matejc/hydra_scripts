8f688d9cd3600a08f330d8476429af12ce0acaf8
Matej Cotman <cotman.matej@gmail.com>
2014-03-15 18:15:38 +0100

    Update vm_build_repo.nix

--- release/vm_build_repo.nix
+++ release/vm_build_repo.nix
@@ -56,7 +56,7 @@ let
     parsed_buildins = (map (n: pkgs.lib.getAttrFromPath (pkgs.lib.splitString "." n) pkgs) (pkgs.lib.splitString " " build_inputs_str));
     build_env = pkgs.buildEnv {
       name = "build_env."+system;
-      paths = parsed_buildins ++ [ pkgs.tree pkgs.stdenv pkgs.binutils pkgs.git pkgs.perl pkgs.cacert ] ++ (if with_vnc_command == "" then [] else [ pkgs.nix pkgs.tightvnc pkgs.xorg.fontmiscmisc pkgs.xorg.fontcursormisc pkgs.busybox pkgs.xlibs.libX11 pkgs.xorg.xorgserver ]);
+      paths = parsed_buildins ++ [ pkgs.tree pkgs.which pkgs.stdenv pkgs.binutils pkgs.git pkgs.perl pkgs.cacert ] ++ (if with_vnc_command == "" then [] else [ pkgs.nix pkgs.tightvnc pkgs.xorg.fontmiscmisc pkgs.xorg.fontcursormisc pkgs.busybox pkgs.xlibs.libX11 pkgs.xorg.xorgserver ]);
       pathsToLink = [ "/" ];
       ignoreCollisions = true;
     };
