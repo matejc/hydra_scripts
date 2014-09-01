3e4c685de9a60d53f976f568158ff57e3c702441
Matej Cotman <cotman.matej@gmail.com>
2014-09-01 21:17:28 +0200

    Update release-mk902.nix

--- release/release-mk902.nix
+++ release/release-mk902.nix
@@ -81,7 +81,7 @@ let
     kernelTarget = "zImage";
     uboot = null;
     gcc = {
-      arch = "armv7";
+      arch = "armv7a";
       fpu = "vfp";
       float = "hard";
     };
@@ -98,7 +98,7 @@ let
     platform = platform;
     openssl.system = "linux-generic32";
     gcc = {
-      arch = "armv7";
+      arch = "armv7a";
       fpu = "vfp";
       float = "softfp";
       abi = "aapcs-linux";
