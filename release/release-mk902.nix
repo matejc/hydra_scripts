cdfe79abb25aad01f103bcf2560bbabd6f4e99fa
Matej Cotman <cotman.matej@gmail.com>
2014-09-01 19:30:36 +0200

    Create release-mk902.nix

---
+++ release/release-mk902.nix
@@ -0,0 +1,123 @@
+{ nixpkgs
+, system ? builtins.currentSystem
+}:
+
+let
+
+  platform = {
+    name = "mk902";
+    kernelMajor = "2.6";
+    kernelHeadersBaseConfig = "kirkwood_defconfig";
+    kernelBaseConfig = "bcmrpi_defconfig";
+    kernelArch = "arm";
+    kernelAutoModules = false;
+    kernelExtraConfig =
+      ''
+        BLK_DEV_RAM y
+        BLK_DEV_INITRD y
+        BLK_DEV_CRYPTOLOOP m
+        BLK_DEV_DM m
+        DM_CRYPT m
+        MD y
+        REISERFS_FS m
+        BTRFS_FS y
+        XFS_FS m
+        JFS_FS y
+        EXT4_FS y
+
+        IP_PNP y
+        IP_PNP_DHCP y
+        NFS_FS y
+        ROOT_NFS y
+        TUN m
+        NFS_V4 y
+        NFS_V4_1 y
+        NFS_FSCACHE y
+        NFSD m
+        NFSD_V2_ACL y
+        NFSD_V3 y
+        NFSD_V3_ACL y
+        NFSD_V4 y
+        NETFILTER y
+        IP_NF_IPTABLES y
+        IP_NF_FILTER y
+        IP_NF_MATCH_ADDRTYPE y
+        IP_NF_TARGET_LOG y
+        IP_NF_MANGLE y
+        IPV6 m
+        VLAN_8021Q m
+
+        CIFS y
+        CIFS_XATTR y
+        CIFS_POSIX y
+        CIFS_FSCACHE y
+        CIFS_ACL y
+
+        ZRAM m
+
+        # Fail to build
+        DRM n
+        SCSI_ADVANSYS n
+        USB_ISP1362_HCD n
+        SND_SOC n
+        SND_ALI5451 n
+        FB_SAVAGE n
+        SCSI_NSP32 n
+        ATA_SFF n
+        SUNGEM n
+        IRDA n
+        ATM_HE n
+        SCSI_ACARD n
+        BLK_DEV_CMD640_ENHANCED n
+
+        FUSE_FS m
+
+        # nixos mounts some cgroup
+        CGROUPS y
+
+        # Latencytop 
+        LATENCYTOP y
+      '';
+    kernelTarget = "zImage";
+    uboot = null;
+    gcc = {
+      arch = "armv7l";
+      fpu = "vfp";
+      float = "hard";
+    };
+  };
+
+  crosssystem = {
+    config = "arm-unknown-linux-uclibc";
+    bigEndian = false;
+    arch = "arm";
+    float = "hard";
+    fpu = "vfp";
+    withTLS = true;
+    libc = "glibc";
+    platform = platform;
+    openssl.system = "linux-generic32";
+    gcc = {
+      arch = "armv7l";
+      fpu = "vfp";
+      float = "softfp";
+      abi = "aapcs-linux";
+    };
+  };
+  pkgsFun = import <nixpkgs>;
+  pkgsNoParams = pkgsFun {};
+  pkgs = pkgsFun {
+    crossSystem = crosssystem;
+    config = pkgs: {
+      packageOverrides = pkgs : {
+        distccMasquerade = pkgs.distccMasquerade.override {
+          gccRaw = pkgs.gccCrossStageFinal.gcc;
+          binutils = pkgs.binutilsCross;
+        };
+      };
+    };
+  };
+
+  jobs.build = pkgs.bash;
+
+in jobs
