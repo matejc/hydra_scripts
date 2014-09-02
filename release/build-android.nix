51ca3277de3ad15f656b8771762e56823d2f5d09
Matej Cotman <cotman.matej@gmail.com>
2014-09-02 22:57:19 +0200

    Update build-android.nix

--- release/build-android.nix
+++ release/build-android.nix
@@ -113,8 +113,7 @@ let
       stateDir = prefix+"/var/nix";
     };
     packageOverrides = pkgs : {
-      python27 = pkgs.stdenv.lib.overrideDerivation pkgs.python27 (oldAttrs : {
-        crossAttrs = {
+      python27.crossDrv = pkgs.stdenv.lib.overrideDerivation pkgs.python27 (oldAttrs : {
           configureFlags = oldAttrs.configureFlags + " --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no ac_cv_have_long_long_format=yes";
           src = pkgs.fetchurl {
             url = "http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tar.xz";
@@ -127,7 +126,6 @@ let
             mv Parser/pgen Parser/pgen_for_build
             patch -p3 < "${hydra_scripts}/patches/Python-2.7.5-xcompile.patch"
           '';
-        };
       });
     };
   };
