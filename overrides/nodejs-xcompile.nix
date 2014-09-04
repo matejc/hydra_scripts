529503073a827d8eebe45bc20036f4c03b3582bd
Matej Cotman <cotman.matej@gmail.com>
2014-09-04 11:13:28 +0200

    Update nodejs-xcompile.nix

--- overrides/nodejs-xcompile.nix
+++ overrides/nodejs-xcompile.nix
@@ -16,7 +16,7 @@ let
 
     # disabled system v8 because v8 3.14 no longer receives security fixes
     # we fall back to nodejs' internal v8 copy which receives backports for now
-    inherit v8;
+    #inherit v8
   };
 
   sharedConfigureFlags = name: [
@@ -30,6 +30,12 @@ in stdenv.mkDerivation {
   name = "nodejs-${version}";
 
   crossAttrs = rec {
+    deps = {
+      openssl = openssl.crossDrv;
+      zlib = zlib.crossDrv;
+      http-parser = http-parser.crossDrv;
+      cares = c-ares.crossDrv;
+    };
     configureFlags = concatMap sharedConfigureFlags (builtins.attrNames deps);
     configurePhase = ''
       ./configure --prefix=$out ${toString configureFlags} --without-snapshot --dest-cpu=arm --dest-os=linux
