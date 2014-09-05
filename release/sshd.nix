983a773fe4a44adfddb472320678630bb03467ff
Matej Cotman <cotman.matej@gmail.com>
2014-09-05 18:56:46 +0200

    Update sshd.nix

--- release/sshd.nix
+++ release/sshd.nix
@@ -1,4 +1,4 @@
-{ pkgs, openssh, bash, utillinux, prefix }:
+{ pkgs, openssh, bash, utillinux, coreutils, prefix }:
 let
 
   sshd_config = pkgs.writeText "sshd_config" ''
@@ -13,25 +13,35 @@ let
 
   sshd_init = pkgs.writeScript "sshd_init.sh" ''
   #!${bash}/bin/bash
+  source ${env}
   mkdir -p ${prefix}/etc/ssh
-  test -f ${prefix}/etc/ssh/ssh_host_rsa_key || ${openssh}/bin/ssh-keygen -t rsa -f ${prefix}/etc/ssh/ssh_host_rsa_key -N ""
-  test -f ${prefix}/etc/ssh/ssh_host_ecdsa_key || ${openssh}/bin/ssh-keygen -t ecdsa -f ${prefix}/etc/ssh/ssh_host_ecdsa_key -N ""
-  test -f ${prefix}/etc/ssh/ssh_host_dsa_key || ${openssh}/bin/ssh-keygen -t dsa -f ${prefix}/etc/ssh/ssh_host_dsa_key -N ""
+  test -f ${prefix}/etc/ssh/ssh_host_rsa_key || ssh-keygen -t rsa -f ${prefix}/etc/ssh/ssh_host_rsa_key -N ""
+  test -f ${prefix}/etc/ssh/ssh_host_ecdsa_key || ssh-keygen -t ecdsa -f ${prefix}/etc/ssh/ssh_host_ecdsa_key -N ""
+  test -f ${prefix}/etc/ssh/ssh_host_dsa_key || ssh-keygen -t dsa -f ${prefix}/etc/ssh/ssh_host_dsa_key -N ""
   test -f ${prefix}/etc/ssh/sshd_config || ln -sv ${sshd_config} ${prefix}/etc/ssh/sshd_config
   '';
 
   sshd_run = pkgs.writeScript "sshd_run.sh" ''
   #!${bash}/bin/bash
+  source ${env}
   test -d ${prefix}/etc/ssh || ${sshd_init}
   mkdir -p ${prefix}/run
-  ${openssh}/sbin/sshd -f ${prefix}/etc/ssh/sshd_config
+  sshd -f ${prefix}/etc/ssh/sshd_config
   '';
 
   sshd_kill = pkgs.writeScript "sshd_kill.sh" ''
   #!${bash}/bin/bash
-  test -f ${prefix}/run/sshd.pid && ${utillinux}/bin/kill -9 `cat ${prefix}/run/sshd.pid`
+  source ${env}
+  test -f ${prefix}/run/sshd.pid && kill -9 `cat ${prefix}/run/sshd.pid`
   '';
+  
+  env = pkgs.writeScript "env.sh" ''
+  #!${bash}/bin/bash
+  export PATH="${utillinux}/bin:${bash}/bin:${openssh}/bin:${openssh}/sbin:${coreutils}/bin"
 
+  "$@"
+  '';
+  
   sshd = pkgs.stdenv.mkDerivation rec {
     name = "sshd";
     unpackPhase = "true";
