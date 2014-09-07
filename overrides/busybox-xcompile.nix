a82887fc9225288535bf5d93014fece9e3b0f530
Matej Cotman <cotman.matej@gmail.com>
2014-09-07 04:18:44 +0200

    Update busybox-xcompile.nix

--- overrides/busybox-xcompile.nix
+++ overrides/busybox-xcompile.nix
@@ -1,5 +1,5 @@
 {stdenv, fetchurl, enableStatic ? false, extraConfig ? ""
-, etcDir ? null
+, etcDir ? null, findutils
 }:
 
 let
@@ -52,7 +52,7 @@ stdenv.mkDerivation rec {
 
   configurePhase = stdenv.lib.optionalString (etcDir != null) ''
     echo "Rewriting /etc to ${etcDir}"
-    find . -type f -exec sed -i -e 's|/etc|${etcDir}|g' {} \;
+    ${findutils}/bin/find . -type f -exec sed -i -e 's|/etc|${etcDir}|g' {} \;
   '' + ''
     make defconfig
     ${configParser}
