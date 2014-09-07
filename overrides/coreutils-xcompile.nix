6ae8d39b4a3d4dd833cc421e74f55ce10de5a79e
Matej Cotman <cotman.matej@gmail.com>
2014-09-07 04:55:32 +0200

    Update coreutils-xcompile.nix

--- overrides/coreutils-xcompile.nix
+++ overrides/coreutils-xcompile.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchurl, perl, gmp ? null
 , aclSupport ? false, acl ? null
 , selinuxSupport? false, libselinux ? null, libsepol ? null
-, etcDir ? null, pkgs, busybox
+, etcDir ? null, pkgs, findutils
 }:
 
 assert aclSupport -> acl != null;
@@ -35,9 +35,9 @@ let
 
       preConfigure = stdenv.lib.optionalString (etcDir != null) ''
         echo "Rewriting /etc/passwd to ${etcDir}/passwd"
-        ${busybox}/bin/find . -type f -exec sed -i -e 's|/etc/passwd|${etcDir}/passwd|g' {} \;
+        ${findutils}/bin/find . -type f -exec sed -i -e 's|/etc/passwd|${etcDir}/passwd|g' {} \;
         echo "Rewriting /etc/group to ${etcDir}/group"
-        ${busybox}/bin/find . -type f -exec sed -i -e 's|/etc/group|${etcDir}/group|g' {} \;
+        ${findutils}/bin/find . -type f -exec sed -i -e 's|/etc/group|${etcDir}/group|g' {} \;
       '';
 
       buildPhase = ''
