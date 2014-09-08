da23a800cf4f5fb2cd6fc25b46468c4127ba53b9
Matej Cotman <cotman.matej@gmail.com>
2014-09-08 16:44:26 +0200

    Update build-android.nix

--- release/build-android.nix
+++ release/build-android.nix
@@ -134,7 +134,7 @@ let
           ${pkgsNoOverrides.findutils}/bin/find . -type f -exec sed -i -e 's|/etc/shadow|${etcDir}/shadow|g' {} \;
         '' + oldAttrs.preConfigure;
       });
-      glibc.crossDrv = pkgs.lib.overrideDerivation (pkgs.glibc.crossDrv) (oldAttrs: {
+      glibc = pkgs.lib.overrideDerivation (pkgs.glibc) (oldAttrs: {
         preConfigure = ''
           ${pkgsNoOverrides.findutils}/bin/find . -type f -iname "*.c" -exec sed -i -e 's|/etc/passwd|${etcDir}/passwd|g' {} \;
           ${pkgsNoOverrides.findutils}/bin/find . -type f -iname "*.c" -exec sed -i -e 's|/etc/group|${etcDir}/group|g' {} \;
