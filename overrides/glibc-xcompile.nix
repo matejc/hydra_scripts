4942f53fed722df4afb6b4d8dc2f5efb769ce002
Matej Cotman <cotman.matej@gmail.com>
2014-09-09 07:44:38 +0200

    Update glibc-xcompile.nix

--- overrides/glibc-xcompile.nix
+++ overrides/glibc-xcompile.nix
@@ -77,14 +77,9 @@ in
   (if cross != null
    then {
       preConfigure = ''
-        echo "#################################################################################################"
-        ls -la ../$sourceRoot
-        echo "#################################################################################################"
-        ls -la .
-        echo "#################################################################################################"
-        ${pkgs.findutils}/bin/find . -type f -iname "*.c" -exec sed -i -e 's|/etc/passwd|${etcDir}/passwd|g' {} \;
-        ${pkgs.findutils}/bin/find . -type f -iname "*.c" -exec sed -i -e 's|/etc/group|${etcDir}/group|g' {} \;
-        ${pkgs.findutils}/bin/find . -type f -iname "*.c" -exec sed -i -e 's|/etc/shadow|${etcDir}/shadow|g' {} \;
+        ${pkgs.findutils}/bin/find ../$sourceRoot -type f -iname "*.c" -exec sed -i -e 's|/etc/passwd|${etcDir}/passwd|g' {} \;
+        ${pkgs.findutils}/bin/find ../$sourceRoot -type f -iname "*.c" -exec sed -i -e 's|/etc/group|${etcDir}/group|g' {} \;
+        ${pkgs.findutils}/bin/find ../$sourceRoot -type f -iname "*.c" -exec sed -i -e 's|/etc/shadow|${etcDir}/shadow|g' {} \;
 
         sed -i s/-lgcc_eh//g "../$sourceRoot/Makeconfig"
 
