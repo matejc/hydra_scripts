cbd4da43f2840a883b1a2f68c6da8fc6064ca0b6
Matej Cotman <cotman.matej@gmail.com>
2014-09-10 01:34:24 +0200

    Update replaceme.nix

--- release/replaceme.nix
+++ release/replaceme.nix
@@ -13,7 +13,7 @@ let
   busybox test -d ${prefix}/tmp || mkdir -p ${prefix}/tmp
   url="${url}"
   domain=`echo $url | busybox sed 's-^[^/]*/*\([^/]*\)/\?.*$-\1-'`
-  ipaddr=`${ping} -c 1 $domain | busybox sed -n 's@^.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*$@\1@p' | busybox head -1`
+  ipaddr=`ping -c 1 $domain | busybox sed -n 's@^.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*$@\1@p' | busybox head -1`
   req_url=`echo $url | busybox sed "s-/[^/]\+-/$ipaddr-"`
   busybox wget $req_url -O ${prefix}/tmp/out.tar.xx
   rm -rf ${prefix}/store || true
