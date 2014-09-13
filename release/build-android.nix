2cf6a796d08846c6945e76e1b849a79c3c6e0172
Matej Cotman <cotman.matej@gmail.com>
2014-09-14 01:08:02 +0200

    Update build-android.nix

--- release/build-android.nix
+++ release/build-android.nix
@@ -125,7 +125,7 @@ let
       nix.crossDrv = pkgs.lib.overrideDerivation pkgs.nix.crossDrv (oldAttrs: {
         nativeBuildInputs = [ pkgs.pkgconfig ];
         buildInputs = with pkgs; [ perl curl openssl boehmgc sqlite ];
-      };
+      });
       bashInteractive = pkgs.bashInteractive.override { interactive = true; readline = pkgs.readline; };
       python27 = pkgs.callPackage ../overrides/python-xcompile.nix { inherit hydra_scripts; };
       bison3 = pkgs.callPackage ../overrides/bison3-xcompile.nix { };
