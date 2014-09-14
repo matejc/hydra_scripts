2bf55b72dbe4fd40fd17e0cc0426ac5af61f0344
Matej Cotman <cotman.matej@gmail.com>
2014-09-14 13:37:43 +0200

    Update perl-cross.nix

--- overrides/perl-cross.nix
+++ overrides/perl-cross.nix
@@ -22,6 +22,11 @@ in
 
       substituteInPlace ./configure --replace "/bin/bash" "${stdenv.shell}"
       substituteInPlace ./cnf/configure --replace "/bin/bash" "${stdenv.shell}"
+      
+      echo "########################################################################"
+      ls -lah ${stdenv.gcc}/bin
+      ls -lah ${stdenv.cross.gcc}/bin
+      exit 1
     '';
 
     configureFlags = [
