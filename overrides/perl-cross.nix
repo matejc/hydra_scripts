b974e39fcb198d82815da61c0a8cfb88fae44afa
Matej Cotman <cotman.matej@gmail.com>
2014-09-14 18:11:23 +0200

    Update perl-cross.nix

--- overrides/perl-cross.nix
+++ overrides/perl-cross.nix
@@ -36,7 +36,6 @@ in
     preBuild = ''
       substituteInPlace ./Makefile.config.SH --replace "#!/bin/bash" "#!${stdenv.shell}"
       substituteInPlace ./miniperl_top --replace "#!/bin/bash" "#!${stdenv.shell}"
-      export CPATH="${stdenv.glibc}/include"
     '';
 
     installPhase = ''
