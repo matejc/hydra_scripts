5bf6599af35dc184863d3eae2d0d308eb3a9da72
Matej Cotman <cotman.matej@gmail.com>
2014-09-14 03:59:03 +0200

    Create perl-cross.nix

---
+++ overrides/perl-cross.nix
@@ -0,0 +1,22 @@
+{ stdenv, fetchgit, perl ? perl520, lib, prefix ? "" }:
+let
+  perlCrossSrc = fetchgit {
+    url = https://github.com/arsv/perl-cross;
+    rev = "refs/tags/0.9.1";
+    sha256 = "68732b270864cb0fc04c5790a594fc3ca420459a0555084cdd65bec9d9674d4a";
+  };
+
+in
+  lib.overrideDerivation perl (oldAttrs: {
+    preConfigure = ''
+      cp -rv ${perlCrossSrc}/* .
+    '' + oldAttrs.preConfigure;
+    configureFlags = [
+      "--target=${stdenv.cross.config}"
+      "-Uinstallusrbinperl"
+      "-Dinstallstyle=lib/perl5"
+      "-Duseshrplib"
+      "-Dlocincpth=${prefix}/usr/include"
+      "-Dloclibpth=${prefix}/usr/lib"
+    ];
+  });
