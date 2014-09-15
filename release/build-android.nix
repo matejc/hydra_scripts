b82942305fcd93690f22bf07abc7b5ec3b22048e
Matej Cotman <cotman.matej@gmail.com>
2014-09-15 19:38:56 +0200

    Update build-android.nix

--- release/build-android.nix
+++ release/build-android.nix
@@ -125,7 +125,7 @@ let
       perlCross = pkgs.callPackage ../overrides/perl-cross.nix { inherit prefix glibcCross; };
       nix.crossDrv = pkgs.lib.overrideDerivation pkgs.nix.crossDrv (oldAttrs: {
         nativeBuildInputs = with pkgs; [ perl pkgconfig ];
-        buildInputs = with pkgs; [ perlCross curl openssl boehmgc sqlite ];
+        buildInputs = with pkgs; [ perlCross curl openssl boehmgc sqlite bzip2 ];
       });
       bashInteractive = pkgs.bashInteractive.override { interactive = true; readline = pkgs.readline; };
       python27 = pkgs.callPackage ../overrides/python-xcompile.nix { inherit hydra_scripts; };
