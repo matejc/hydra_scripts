57cc68ce81e7e693fce53198390e9705e29c300a
Matej Cotman <cotman.matej@gmail.com>
2014-09-19 17:06:31 +0200

    Update build-android.nix

--- release/build-android.nix
+++ release/build-android.nix
@@ -133,7 +133,7 @@ let
       }));
       #perl_xcompile = pkgs.callPackage ../overrides/perl-cross.nix { inherit prefix glibcCross; binutils = binutils_xcompile; };
       perlCross = pkgs.forceNativeDrv (pkgs.callPackage ../overrides/perl-cross.nix { inherit prefix glibcCross; });
-      buildPerlCrossPackage = import ../overrides/buildPerlPackage-cross.nix pkgs.perl520 perlCross glibcCross pkgs busybox;
+      buildPerlCrossPackage = import ../overrides/buildPerlPackage-cross.nix (pkgs.makeStdenvCross pkgs.stdenv crosssystem binutilsCross pkgs.gccCrossStageStatic) pkgs.perl520 perlCross glibcCross pkgs busybox;
       perlCrossPackages = import "${pkgs.path}/pkgs/top-level/perl-packages.nix" {
         pkgs = pkgs // {
           perl = pkgs.perl520;
