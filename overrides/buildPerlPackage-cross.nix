a37e58358f64467c43ac83839b85a8feeae4c2c2
Matej Cotman <cotman.matej@gmail.com>
2014-09-21 17:40:51 +0200

    Update buildPerlPackage-cross.nix

--- overrides/buildPerlPackage-cross.nix
+++ overrides/buildPerlPackage-cross.nix
@@ -45,13 +45,15 @@ perl.stdenv.mkDerivation (
 
       rm $GCCBIN/gcc
       echo -e "#!${pkgs.stdenv.shell} -x\n\
-      ${pkgs.gccCrossStageStatic}/bin/${pkgs.stdenv.cross.config}-gcc -Wl,-dynamic-linker,$INTERPRETER \$(echo \$@ | sed -e 's|-D_FILE_OFFSET_BITS=64||g' -e 's|-D_LARGEFILE64_SOURCE||g' -e 's|-D_LARGEFILE_SOURCE||g' -e 's|${perlCross.stdenv.gcc.libc}|${glibcCross}|g' ${sedCrossDrvs buildInputsOrg} -e 's|$PERLLIBDIR|$PERLCROSSLIBDIR|g')" > $GCCBIN/gcc
+      ${pkgs.gccCrossStageStatic}/bin/${pkgs.stdenv.cross.config}-gcc -Wl,-dynamic-linker,$INTERPRETER \$(echo \$@ | -e 's|${perlCross.stdenv.gcc.libc}|${glibcCross}|g' ${sedCrossDrvs buildInputsOrg} -e 's|$PERLLIBDIR|$PERLCROSSLIBDIR|g')" > $GCCBIN/gcc
       chmod +x $GCCBIN/gcc
 
       rm $GCCBIN/ld
       echo -e "#!${pkgs.stdenv.shell} -x\n\
       ${pkgs.gccCrossStageStatic}/bin/${pkgs.stdenv.cross.config}-ld \$(echo \$@ | sed -e 's|-Wl,-Bsymbolic|-Bsymbolic|g' -e 's|${perlCross.stdenv.gcc.libc}|${glibcCross}|g' ${sedCrossDrvs buildInputsOrg} -e 's|$PERLLIBDIR|$PERLCROSSLIBDIR|g')" > $GCCBIN/ld
       chmod +x $GCCBIN/ld
+
+      sed -i -e 's|-D_FILE_OFFSET_BITS=64||g' -e 's|-D_LARGEFILE64_SOURCE||g' -e 's|-D_LARGEFILE_SOURCE||g' ./Makefile
     '';
     
     postInstall = ''
