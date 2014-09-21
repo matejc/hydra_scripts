747cfca6eda6f5b0cbc472402c02478df5f2652a
Matej Cotman <cotman.matej@gmail.com>
2014-09-21 23:49:43 +0200

    Update buildPerlPackage-cross.nix

--- overrides/buildPerlPackage-cross.nix
+++ overrides/buildPerlPackage-cross.nix
@@ -45,7 +45,7 @@ perl.stdenv.mkDerivation (
 
       rm $GCCBIN/gcc
       echo -e "#!${pkgs.stdenv.shell} -x\n\
-      ${pkgs.gccCrossStageStatic}/bin/${pkgs.stdenv.cross.config}-gcc $GCC_EXTRA_OPTIONS -Wl,-dynamic-linker,$INTERPRETER \$(echo \$@ | sed -e 's|${perlCross.stdenv.gcc.libc}|${glibcCross}|g' ${sedCrossDrvs buildInputsOrg} -e 's|$PERLLIBDIR|$PERLCROSSLIBDIR|g')" > $GCCBIN/gcc
+      ${pkgs.gccCrossStageStatic}/bin/${pkgs.stdenv.cross.config}-gcc $GCC_EXTRA_OPTIONS -lc -Wl,-dynamic-linker,$INTERPRETER \$(echo \$@ | sed -e 's|${perlCross.stdenv.gcc.libc}|${glibcCross}|g' ${sedCrossDrvs buildInputsOrg} -e 's|$PERLLIBDIR|$PERLCROSSLIBDIR|g')" > $GCCBIN/gcc
       chmod +x $GCCBIN/gcc
 
       rm $GCCBIN/ld
