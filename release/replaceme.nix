97ffcdf30562e75a322598b41db626a3acb5e09f
Matej Cotman <cotman.matej@gmail.com>
2014-09-22 23:54:07 +0200

    Update replaceme.nix

--- release/replaceme.nix
+++ release/replaceme.nix
@@ -4,11 +4,12 @@ let
   #!${shell}
   export PATH="${path}:$PATH"
   mkdir -p ${prefix}/tmp
-  url="${url}"
-  domain=`echo $url | busybox sed 's-^[^/]*/*\([^/]*\)/\?.*$-\1-'`
-  ipaddr=`ping -c 1 $domain | busybox sed -n 's@^.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*$@\1@p' | busybox head -1`
-  req_url=`echo $url | busybox sed "s-/[^/]\+-/$ipaddr-"`
-  busybox wget $req_url -O ${prefix}/tmp/out.tar.xx
+  #url="${url}"
+  #domain=`echo $url | busybox sed 's-^[^/]*/*\([^/]*\)/\?.*$-\1-'`
+  #ipaddr=`ping -c 1 $domain | busybox sed -n 's@^.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*$@\1@p' | busybox head -1`
+  #req_url=`echo $url | busybox sed "s-/[^/]\+-/$ipaddr-"`
+  #busybox wget $req_url -O ${prefix}/tmp/out.tar.xx
+  busybox wget ${url} -O ${prefix}/tmp/out.tar.xx
   rm -rf ${prefix}/store || true
   rm -rf ${prefix}/result || true
   busybox tar xvf ${prefix}/tmp/out.tar.xx -C /
