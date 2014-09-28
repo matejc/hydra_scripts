9ca73d311a493d5edbafdb3f4c2b1ed05f5e89ed
Matej Cotman <cotman.matej@gmail.com>
2014-09-28 09:12:20 +0200

    Update replaceme.nix

--- release/replaceme.nix
+++ release/replaceme.nix
@@ -2,21 +2,15 @@
 let
   replaceme = pkgs.writeScriptBin "replaceme" ''
   #!${shell}
-
   export PATH="${resultPath}"
   mkdir -p ${prefix}/tmp
-  busybox wget ${url} -O ${prefix}/tmp/out.tar.xx
-
-  #url="${url}"
-  #domain=`echo $url | busybox sed 's-^[^/]*/*\([^/]*\)/\?.*$-\1-'`
-  #ipaddr=`ping -c 1 $domain | busybox sed -n 's@^.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*$@\1@p' | busybox head -1`
-  #req_url=`echo $url | busybox sed "s-/[^/]\+-/$ipaddr-"`
-  #busybox wget $req_url -O ${prefix}/tmp/out.tar.xx
-
-  export PATH="${systemPath}"
-  rm -rf ${prefix}/store || true
-  rm -rf ${prefix}/result || true
-  busybox tar xvf ${prefix}/tmp/out.tar.xx -C /
+  busybox wget ${url} -O ${prefix}/tmp/out.tar.xx && \
+  { \
+    export PATH="${systemPath}" && \
+    rm -rf ${prefix}/store || true && \
+    rm -rf ${prefix}/result || true && \
+    busybox tar xvf ${prefix}/tmp/out.tar.xx -C /; \
+  }
   '';
 in
   replaceme
