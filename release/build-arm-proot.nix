5529b7d7eb5f63ca3c5bc3d4f78fd3808cea7ccf
Matej Cotman <cotman.matej@gmail.com>
2014-12-02 20:19:00 +0100

    Update build-arm-proot.nix

--- release/build-arm-proot.nix
+++ release/build-arm-proot.nix
@@ -170,7 +170,7 @@ let
 
   build = {
     vmEnvironment = pkgs.buildEnv {
-      name = "outenv-${pkgs.stdenv.cross.config}";
+      name = "outenv-arm";
       paths = paths ++ [mybash] ++ (pkgs.lib.optionals (build_hydra == "1") [hydra]) ++ (pkgs.lib.optionals (build_sshd == "1") [sshd]) ++ (pkgs.lib.optionals (replaceme_url != "") [replaceme]);
       pathsToLink = [ "/" ];
       ignoreCollisions = true;
