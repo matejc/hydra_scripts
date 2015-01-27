4ca4c1625797b28d020c413cb3ece681c58d3346
Matej Cotman <cotman.matej@gmail.com>
2015-01-27 21:13:57 +0100

    Update just-run-it.nix

--- release/nixui/just-run-it.nix
+++ release/nixui/just-run-it.nix
@@ -4,30 +4,36 @@ let
   inherit (pkgs) stdenv fetchgit nix makeDesktopItem writeScript;
   node_webkit = <src/node-webkit.nix>;
   nixui = (import <src/default.nix> { inherit pkgs; }).build;
-  script = writeScript "nixui" ''
-    #! ${stdenv.shell}
-    export PATH="${nix}/bin:\$PATH"
-    ${node_webkit}/bin/nw ${nixui}/lib/node_modules/nixui/
-  '';
 
   jobs = {
     nixui = stdenv.mkDerivation rec {
       name = "nixui-dev";
       inherit src;
-      buildInputs = with pkgs; [ gnumake nix ];
+      buildInputs = with pkgs; [ gnumake nix busybox tightvnc ];
       configurePhase = ''
         export NIX_REMOTE=daemon
         export NIX_PATH="nixpkgs=${nixpkgs}"
       '';
       buildPhase = ''
-        make just-run-it
+        nix-build dispatcher.nix --argstr action package
       '';
       checkPhase = ''
+        # this command runs the VNC server on screen :99
+        vncserver :99
+
+        # start your tests by setting DISPLAY env variable
+        export DISPLAY=:99.0
+        
+        # check if it starts
+        timeout 5 ./result/bin/nixui
+
+        # and then kill the VNC server
+        vncserver -kill :99
+
         make test
       '';
       installPhase = ''
-        mkdir -p $out/bin
-        ln -s ${script} $out/bin/nixui
+        make package
       '';
     };
   };
