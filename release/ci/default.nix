1b2cb84b39f4abd73d7c8583a621cae4c121d66f
Matej Cotman <cotman.matej@gmail.com>
2015-02-03 20:33:35 +0100

    Update default.nix

--- release/ci/default.nix
+++ release/ci/default.nix
@@ -67,10 +67,11 @@ let
     test -w $PROOT_DIR/xchg || chmod -R g+w $PROOT_DIR/xchg
 
     cp ${<nixpkgs/maintainers/scripts/travis-nox-review-pr.sh>} $PROOT_DIR/xchg
+    cp ${buildScript}/bin/build.sh $PROOT_DIR/xchg
 
     { timeout ${timeout} ${pkgs.proot}/bin/proot -S "$PROOT_DIR" \
       -b /bin/sh \
-      ${extraPRootArgs} ${buildScript}/bin/build.sh; } || true
+      ${extraPRootArgs} /xchg/build.sh; } || true
 
     test -w $PROOT_DIR || echo "WARNING: `id` has no write permission for $PROOT_DIR"
     chmod g+w $PROOT_DIR || true
