3933de0d22dd1d92ab62a51916280864e972a71d
Matej Cotman <cotman.matej@gmail.com>
2015-07-14 22:40:38 +0200

    Create linuxPackages_t100pam.nix

---
+++ release/linuxPackages_t100pam.nix
@@ -0,0 +1,21 @@
+{ nixpkgs, system, hydra_scripts }:
+let
+  kernelExtraConfig = builtins.readFile "${hydra_scripts}/";
+  config = {
+    nixpkgs.config = {
+      packageOverrides = pkgs: {
+        stdenv = pkgs.stdenv // {
+          platform = pkgs.stdenv.platform // {
+            inherit kernelExtraConfig;
+            name = "tablet";
+          };
+        };
+      };
+    };
+    boot.kernelPackages = pkgs.linuxPackages_4_1;
+  };
+  pkgs = import <nixpkgs> { inherit system config; };
+  jobs = {
+    kernelPackages = config.boot.kernelPackages;
+  };
+in jobs
