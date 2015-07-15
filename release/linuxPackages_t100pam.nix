700c9e8492d2e449414f0200f18510fbc53f1f12
Matej Cotman <cotman.matej@gmail.com>
2015-07-15 18:43:41 +0200

    Update linuxPackages_t100pam.nix

--- release/linuxPackages_t100pam.nix
+++ release/linuxPackages_t100pam.nix
@@ -1,14 +1,21 @@
 { nixpkgs, system, hydra_scripts }:
 let
-  pkgs = import <nixpkgs> { inherit system; };
+  pkgs = import <nixpkgs> { inherit system config; };
   pkgs2storeContents = l : map (x: { object = x; symlink = "none"; }) l;
   kernelExtraConfig = builtins.readFile "${hydra_scripts}/config/t100pam_extra.config";
   linuxPackages = pkgs.linuxPackages_4_1;
-  stdenv = pkgs.stdenv // {
-    platform = pkgs.stdenv.platform // {
-      inherit kernelExtraConfig;
-      name = "tablet";
+  config = {
+    nixpkgs.config = {
+      packageOverrides = pkgs: {
+        stdenv = pkgs.stdenv // {
+          platform = pkgs.stdenv.platform // {
+            inherit kernelExtraConfig;
+            name = "tablet";
+          };
+        };
+      };
     };
+    boot.kernelPackages = pkgs.linuxPackages_4_1;
   };
   linux = linuxPackages.kernel.override { extraConfig = kernelExtraConfig; };
   tarball = import <nixpkgs/nixos/lib/make-system-tarball.nix> {
@@ -16,7 +23,7 @@ let
     inherit stdenv;
     contents = [];
     extraArgs = "--owner=0";
-    storeContents = (pkgs2storeContents [ linux ]);
+    storeContents = (pkgs2storeContents [ pkgs.config.boot.kernelPackages.kernel ]);
   };
   jobs = {
     inherit tarball;
