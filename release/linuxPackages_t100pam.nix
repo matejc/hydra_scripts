bb7105267d21d80250c0d68039f65e784809ae8b
Matej Cotman <cotman.matej@gmail.com>
2015-07-15 18:30:25 +0200

    Update linuxPackages_t100pam.nix

--- release/linuxPackages_t100pam.nix
+++ release/linuxPackages_t100pam.nix
@@ -3,21 +3,21 @@ let
   pkgs = import <nixpkgs> { inherit system config; };
   kernelExtraConfig = builtins.readFile "${hydra_scripts}/config/t100pam_extra.config";
   linuxPackages = pkgs.linuxPackages_4_1;
-  config = {
-    nixpkgs.config = {
-      packageOverrides = pkgs: {
-        stdenv = pkgs.stdenv // {
-          platform = pkgs.stdenv.platform // {
-            inherit kernelExtraConfig;
-            name = "tablet";
-          };
-        };
-      };
+  stdenv = pkgs.stdenv // {
+    platform = pkgs.stdenv.platform // {
+      inherit kernelExtraConfig;
+      name = "tablet";
     };
-    boot.kernelPackages = linuxPackages;
+  };
+  linux = linuxPackages.kernel.override { extraConfig = kernelExtraConfig; };
+  tarball = import <nixpkgs/nixos/lib/make-system-tarball.nix> {
+    inherit (pkgs) perl xz pathsFromGraph;
+    inherit stdenv;
+    contents = [];
+    extraArgs = "--owner=0";
+    storeContents = (pkgs2storeContents [ linux ]);
   };
   jobs = {
-    #kernelPackages = config.boot.kernelPackages;
-    linux = linuxPackages.kernel.override { extraConfig = kernelExtraConfig; };
+    inherit tarball;
   };
 in jobs
